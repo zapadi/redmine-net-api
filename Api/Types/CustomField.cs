1e5c7d2595476f9d78c87c45491ad7184d88b485
dorin.huzum@gmail.com <dorin.huzum@gmail.com@cde3ca97-435f-4537-8a35-53ad0ef271d5>
2011-09-25 18:51:57 +0000

    initial commit

---
+++ Api/Types/CustomField.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Redmine.Net.Api.Types
+{
+    public class CustomField
+    {
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        /// <value>The id.</value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public String Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>The value.</value>
+        public String Value { get; set; }
+    }
+}
