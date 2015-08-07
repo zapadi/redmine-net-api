e49fa933fa1a48da72db1f6a1eca86efe322b717
Adrian Popescu <adi.popescu@gmail.com>
2015-08-07 12:55:57 +0300

    updated assemblies copyright and description.

--- redmine-net20-api/Properties/AssemblyInfo.cs
+++ redmine-net20-api/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("redmine-net20-api")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("redmine-net-api is a library for communicating with a Redmine project management application.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("redmine-net20-api")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Adrian Popescu 2011 - 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
