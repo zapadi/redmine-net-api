5a4f4192be96875dd7eb4ee0b42486421387380a
Adrian Popescu <adi.popescu@gmail.com>
2015-09-28 10:36:08 +0300

    Added Watcher test

---
+++ UnitTestRedmineNetApi/WatcherTestsJson.cs
@@ -0,0 +1,27 @@
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Redmine.Net.Api;
+
+namespace UnitTestRedmineNetApi
+{
+    [TestClass]
+    public class WatcherTestsJson
+    {
+        private RedmineManager redmineManager;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var uri = ConfigurationManager.AppSettings["uri"];
+            var apiKey = ConfigurationManager.AppSettings["apiKey"];
+            redmineManager = new RedmineManager(uri, apiKey, MimeFormat.json);
+        }
+
+        [TestMethod]
+        public void AddWatcher()
+        {
+            redmineManager.AddWatcher(44, 8);
+            Assert.Inconclusive();
+        }
+    }
+}
