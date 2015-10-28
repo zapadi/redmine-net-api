8abcf475b2bbc89b05f0919c237ec935e17d822f
jhmt <jhashimot00@gmail.com>
2015-10-28 20:09:06 +1300

    Add IncludeParameters flag
    Initial commit to add "IncludeParameter" enum flag

---
+++ redmine-net20-api/Types/IncludeParameters.cs
@@ -0,0 +1,22 @@
+﻿namespace Redmine.Net.Api.Types
+{
+    using System;
+    [Flags]
+    public enum IncludeParameters
+    {
+        none = 0,
+        children = 1 << 0,   // 1
+        attachments = 1 << 1,   // 2
+        relations = 1 << 2,   // 4
+        changesets = 1 << 3,   // 8
+        journals = 1 << 4,   // 16
+        watchers = 1 << 5    // 32
+    }
+    public static class IncludeParametersExtensions
+    {
+        public static bool IsSet(this IncludeParameters source, IncludeParameters flag)
+        {
+            return (source & flag) == flag;
+        }
+    }
+}
