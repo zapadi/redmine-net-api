cac3fee6d15053ae6e46ab737982c86afe4475cd
Alina Chitu <alinaiuliana.chitu@gmail.com>
2016-01-18 21:25:21 +0200

    Modified custom fields tests

--- UnitTest-redmine-net40-api/CustomFieldTests.cs
+++ UnitTest-redmine-net40-api/CustomFieldTests.cs
@@ -14,19 +14,14 @@ namespace UnitTest_redmine_net40_api
     [TestClass]
     public class CustomFieldTests
     {
-        #region Properties
         private RedmineManager redmineManager;
-        private string uri;
-        private string apiKey;
-        #endregion Properties
 
-        #region Initialize
+        private const int NUMBER_OF_CUSTOM_FIELDS = 4;
+        private const bool ISSUE_CUSTOM_FIELD_EXISTS = true;
+
         [TestInitialize]
         public void Initialize()
         {
-            uri = ConfigurationManager.AppSettings["uri"];
-            apiKey = ConfigurationManager.AppSettings["apiKey"];
-
             SetMimeTypeJSON();
             SetMimeTypeXML();
         }
@@ -34,24 +29,27 @@ namespace UnitTest_redmine_net40_api
         [Conditional("JSON")]
         private void SetMimeTypeJSON()
         {
-            redmineManager = new RedmineManager(uri, apiKey, MimeFormat.json);
+            redmineManager = new RedmineManager(Helper.Uri, Helper.ApiKey, MimeFormat.json);
         }
 
         [Conditional("XML")]
         private void SetMimeTypeXML()
         {
-            redmineManager = new RedmineManager(uri, apiKey, MimeFormat.xml);
+            redmineManager = new RedmineManager(Helper.Uri, Helper.ApiKey, MimeFormat.xml);
         }
-        #endregion Initialize
-
-        #region Tests
+ 
         [TestMethod]
         public void RedmineCustomFields_ShouldGetAllCustomFields()
         {
             var customFields = redmineManager.GetObjects<CustomField>(null);
 
-            Assert.IsNotNull(customFields);
+            Assert.IsNotNull(customFields, "Get custom fields returned null.");
+            Assert.IsTrue(customFields.Count == NUMBER_OF_CUSTOM_FIELDS, "Custom fields count != "+NUMBER_OF_CUSTOM_FIELDS);
+            CollectionAssert.AllItemsAreNotNull(customFields, "Custom fields list contains null items.");
+            CollectionAssert.AllItemsAreUnique(customFields, "Custom fields items are not unique.");
+            CollectionAssert.AllItemsAreInstancesOfType(customFields, typeof(CustomField), "Not all items are of type custom fields.");
+
+            Assert.IsTrue(customFields.Exists(cf => cf.CustomizedType == RedmineKeys.ISSUE) == ISSUE_CUSTOM_FIELD_EXISTS, "Customized type check not valid.");
         }
-        #endregion Tests
     }
 }
