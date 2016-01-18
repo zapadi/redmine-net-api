4e1d3bb341105dff9e15f9eae966bd8026cd063d
Alina Chitu <alinaiuliana.chitu@gmail.com>
2016-01-18 23:33:05 +0200

    Group unit tests refactoring

--- UnitTest-redmine-net40-api/GroupTests.cs
+++ UnitTest-redmine-net40-api/GroupTests.cs
@@ -5,41 +5,35 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Redmine.Net.Api;
 using Redmine.Net.Api.Types;
 using System.Diagnostics;
+using System.Linq;
 
-namespace UnitTestRedmineNetApi
+namespace UnitTest_redmine_net40_api
 {
     [TestClass]
-    public class UnitTest_redmine_net40_api
+    public class GroupTests
     {
-        #region Constants
-        private const string groupId = "9";
-        private const string groupName = "Administrators";
+        private RedmineManager redmineManager;
+
+        private const int NUMBER_OF_GROUPS = 2;
+        private const string GROUP_ID = "34";
+        private const string GROUP_NAME = "Test";
+        private const int NUMBER_OF_MEMBERSHIPS = 1;
+        private const int NUMBER_OF_USERS = 2;
 
         //group data - used for create
-        private const string newGroupName = "Developers";
-        private const int newGroupUserId = 8;
+        private const string NEW_GROUP_NAME = "Developers";
+        private const int NEW_GROUP_USER_ID = 5;
 
         //data used for update
-        private const string updatedGroupId = "31";
-        private const string updatedGroupName = "Best Developers";
-        private const int updatedGroupUserId = 2;
+        private const string UPDATED_GROUP_ID = "36";
+        private const string UPDATED_GROUP_NAME = "Best Developers";
+        private const int UPDATED_GROUP_USER_ID = 1;
 
-        private const string groupIdToDelete = "31";
-        #endregion Constants
+        private const string DELETED_GROUP_ID = "36";
 
-        #region Properties
-        private RedmineManager redmineManager;
-        private string uri;
-        private string apiKey;
-        #endregion Properties
-
-        #region Initialize
         [TestInitialize]
         public void Initialize()
         {
-            uri = ConfigurationManager.AppSettings["uri"];
-            apiKey = ConfigurationManager.AppSettings["apiKey"];
-
             SetMimeTypeJSON();
             SetMimeTypeXML();
         }
@@ -47,64 +41,78 @@ namespace UnitTestRedmineNetApi
         [Conditional("JSON")]
         private void SetMimeTypeJSON()
         {
-            redmineManager = new RedmineManager(uri, apiKey, MimeFormat.json);
+            redmineManager = new RedmineManager(Helper.Uri, Helper.ApiKey, MimeFormat.json);
         }
 
         [Conditional("XML")]
         private void SetMimeTypeXML()
         {
-            redmineManager = new RedmineManager(uri, apiKey, MimeFormat.xml);
+            redmineManager = new RedmineManager(Helper.Uri, Helper.ApiKey, MimeFormat.xml);
         }
-        #endregion Initialize
 
-        #region Tests
         [TestMethod]
-        public void GetAllGroups()
+        public void Should_Get_All_Groups()
         {
-            var result = redmineManager.GetObjects<Group>(null);
+            var groups = redmineManager.GetObjects<Group>(null);
 
-            Assert.IsNotNull(result);
+            Assert.IsNotNull(groups, "Get objects returned null.");
+            CollectionAssert.AllItemsAreInstancesOfType(groups, typeof(Group), "Not all items are of type group.");
+            CollectionAssert.AllItemsAreNotNull(groups, "Groups contains null items.");
+            CollectionAssert.AllItemsAreUnique(groups, "Groups items are not unique.");
+            Assert.IsTrue(groups.Count == NUMBER_OF_GROUPS, "Number of groups != "+NUMBER_OF_GROUPS);
         }
 
         [TestMethod]
-        public void GetGroup_With_Memberships()
+        public void Should_Get_Group_With_Memberships()
         {
-            var result = redmineManager.GetObject<Group>(groupId, new NameValueCollection() { { "include", "memberships" } });
+            var group = redmineManager.GetObject<Group>(GROUP_ID, new NameValueCollection() { { RedmineKeys.INCLUDE, RedmineKeys.MEMBERSHIPS } });
 
-            Assert.IsNotNull(result);
+            Assert.IsNotNull(group, "Get group by id returned null.");
+            CollectionAssert.AllItemsAreInstancesOfType(group.Memberships.ToList(), typeof(Membership), "Not all items are of type membership.");
+            CollectionAssert.AllItemsAreNotNull(group.Memberships.ToList(), "Memberships contains null items.");
+            CollectionAssert.AllItemsAreUnique(group.Memberships.ToList(), "Memberships items are not unique.");
+            Assert.IsTrue(group.Memberships.Count == NUMBER_OF_MEMBERSHIPS, "Number of memberships != " + NUMBER_OF_MEMBERSHIPS);
         }
 
         [TestMethod]
-        public void Should_Return_Group_With_Users()
+        public void Should_Get_Group_With_Users()
         {
-            var result = redmineManager.GetObject<Group>(groupId, new NameValueCollection() { { "include", "users" } });
+            var group = redmineManager.GetObject<Group>(GROUP_ID, new NameValueCollection() { { RedmineKeys.INCLUDE, RedmineKeys.USERS } });
 
-            Assert.IsNotNull(result);
+            Assert.IsNotNull(group, "Get group by id returned null.");
+            CollectionAssert.AllItemsAreInstancesOfType(group.Users.ToList(), typeof(GroupUser), "Not all items are of type GroupUser.");
+            CollectionAssert.AllItemsAreNotNull(group.Users.ToList(), "Users contains null items.");
+            CollectionAssert.AllItemsAreUnique(group.Users.ToList(), "Users items are not unique.");
+            Assert.IsTrue(group.Users.Count == NUMBER_OF_USERS, "Number of users != " + NUMBER_OF_USERS);
         }
 
         [TestMethod]
-        public void GetGroup_WithAll_AssociatedData()
+        public void Should_Get_Group_With_All_Associated_Data()
         {
-            var result = redmineManager.GetObject<Group>(groupId, new NameValueCollection() { { "include", "memberships, users" } });
+            var group = redmineManager.GetObject<Group>(GROUP_ID, new NameValueCollection() { { RedmineKeys.INCLUDE, RedmineKeys.MEMBERSHIPS+","+RedmineKeys.USERS } });
 
-            Assert.IsNotNull(result);
+            Assert.IsNotNull(group, "Get group by id returned null.");
+
+            CollectionAssert.AllItemsAreInstancesOfType(group.Memberships.ToList(), typeof(Membership), "Not all items are of type membership.");
+            CollectionAssert.AllItemsAreNotNull(group.Memberships.ToList(), "Memberships contains null items.");
+            CollectionAssert.AllItemsAreUnique(group.Memberships.ToList(), "Memberships items are not unique.");
+            Assert.IsTrue(group.Memberships.Count == NUMBER_OF_MEMBERSHIPS, "Number of memberships != " + NUMBER_OF_MEMBERSHIPS);
+
+            CollectionAssert.AllItemsAreInstancesOfType(group.Users.ToList(), typeof(GroupUser), "Not all items are of type GroupUser.");
+            CollectionAssert.AllItemsAreNotNull(group.Users.ToList(), "Users contains null items.");
+            CollectionAssert.AllItemsAreUnique(group.Users.ToList(), "Users items are not unique.");
+            Assert.IsTrue(group.Users.Count == NUMBER_OF_USERS, "Number of users != " + NUMBER_OF_USERS);
+
+            Assert.AreEqual(group.Name, GROUP_NAME, "Group name is not valid.");
         }
 
         [TestMethod]
-        public void RedmineGroups_ShouldGetGroupById()
-        {
-            Group adminGroup = redmineManager.GetObject<Group>(groupId, new NameValueCollection { { "include", "users,memberships" } });
-        
-            Assert.AreEqual(adminGroup.Name, groupName);
-        }
-
-        [TestMethod]
-        public void RedmineGroups_ShouldAddGroup()
+        public void Should_Add_Group()
         {
             Group group = new Group();
-            group.Name = newGroupName;
+            group.Name = NEW_GROUP_NAME;
             group.Users = new List<GroupUser>();
-            group.Users.Add(new GroupUser { Id = newGroupUserId });
+            group.Users.Add(new GroupUser { Id = NEW_GROUP_USER_ID });
 
             Group savedGroup = null;
             try
@@ -117,30 +125,37 @@ namespace UnitTestRedmineNetApi
                 return;
             }
 
-            Assert.IsNotNull(savedGroup);
-            Assert.AreEqual(group.Name, savedGroup.Name);
+            Assert.IsNotNull(savedGroup, "Create group returned null.");
+            Assert.AreEqual(group.Name, savedGroup.Name, "Saved group name is not valid.");
         }
 
         [TestMethod]
-        public void RedmineGroups_ShouldUpdateGroup()
+        public void Should_Update_Group()
         {
-            Group group = redmineManager.GetObject<Group>(updatedGroupId, new NameValueCollection { { "include", "users" } });
-            group.Name = updatedGroupName;
-            group.Users.Add(new GroupUser { Id = updatedGroupUserId });
-            redmineManager.UpdateObject<Group>(updatedGroupId, group);
+            Group group = redmineManager.GetObject<Group>(UPDATED_GROUP_ID, new NameValueCollection { { RedmineKeys.INCLUDE, RedmineKeys.USERS} });
+            group.Name = UPDATED_GROUP_NAME;
+            group.Users.Add(new GroupUser { Id = UPDATED_GROUP_USER_ID });
 
-            Group updatedGroup = redmineManager.GetObject<Group>(updatedGroupId, new NameValueCollection { { "include", "users" } });
+            redmineManager.UpdateObject<Group>(UPDATED_GROUP_ID, group);
 
-            Assert.IsTrue(updatedGroup.Users.Find(u => u.Id == 2) != null);
-            Assert.AreEqual(group.Name, updatedGroup.Name);
+            Group updatedGroup = redmineManager.GetObject<Group>(UPDATED_GROUP_ID, new NameValueCollection { { RedmineKeys.INCLUDE, RedmineKeys.USERS } });
+
+            Assert.IsNotNull(updatedGroup, "Get group returned null.");
+            Assert.AreEqual(updatedGroup.Name, UPDATED_GROUP_NAME, "Group name was not updated.");
+
+            Assert.IsNotNull(updatedGroup.Users, "Group users list is null.");
+            CollectionAssert.AllItemsAreInstancesOfType(updatedGroup.Users.ToList(), typeof(GroupUser), "Not all items are of type GroupUser.");
+            CollectionAssert.AllItemsAreNotNull(updatedGroup.Users.ToList(), "Users contains null items.");
+            CollectionAssert.AllItemsAreUnique(updatedGroup.Users.ToList(), "Users items are not unique.");
+            Assert.IsTrue(updatedGroup.Users.Find(u => u.Id == UPDATED_GROUP_USER_ID) != null, "User was not added to group.");
         }
 
         [TestMethod]
-        public void RedmineGroups_ShouldDeleteGroup()
+        public void Should_Delete_Group()
         {
             try
             {
-                redmineManager.DeleteObject<Group>(groupIdToDelete, null);
+                redmineManager.DeleteObject<Group>(DELETED_GROUP_ID, null);
             }
             catch (RedmineException)
             {
@@ -150,7 +165,7 @@ namespace UnitTestRedmineNetApi
 
             try
             {
-                Group group = redmineManager.GetObject<Group>(groupIdToDelete, null);
+                Group group = redmineManager.GetObject<Group>(DELETED_GROUP_ID, null);
             }
             catch (RedmineException exc)
             {
@@ -161,13 +176,12 @@ namespace UnitTestRedmineNetApi
         }
 
         [TestMethod]
-        public void RedmineGroups_ShouldCompareGroups()
+        public void Should_Compare_Groups()
         {
-            var firstGroup = redmineManager.GetObject<Group>(groupId, new NameValueCollection() { { "include", "memberships, users" } });
-            var secondGroup = redmineManager.GetObject<Group>(groupId, new NameValueCollection() { { "include", "memberships, users" } });
+            var firstGroup = redmineManager.GetObject<Group>(GROUP_ID, new NameValueCollection() { { RedmineKeys.INCLUDE, RedmineKeys.MEMBERSHIPS + "," + RedmineKeys.USERS } });
+            var secondGroup = redmineManager.GetObject<Group>(GROUP_ID, new NameValueCollection() { { RedmineKeys.INCLUDE, RedmineKeys.MEMBERSHIPS + "," + RedmineKeys.USERS } });
 
-            Assert.IsTrue(firstGroup.Equals(secondGroup));
+            Assert.IsTrue(firstGroup.Equals(secondGroup), "Compared groups are different.");
         }
-        #endregion Tests
     }
 }
