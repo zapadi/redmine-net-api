81b88da0a01b300ef392ffb3daa045a0586a14d0
Alina Chitu <alinaiuliana.chitu@gmail.com>
2016-01-19 23:20:18 +0200

    Roles unit tests

--- UnitTest-redmine-net40-api/RoleTests.cs
+++ UnitTest-redmine-net40-api/RoleTests.cs
@@ -14,26 +14,16 @@ namespace UnitTest_redmine_net40_api
     [TestClass]
     public class RoleTests
     {
-        #region Constants
-        private const int numberOfRoles = 3;
-
-        private const string roleId = "4";
-        private const string roleName = "Admins";
-        #endregion Constants
-
-        #region Properties
         private RedmineManager redmineManager;
-        private string uri;
-        private string apiKey;
-        #endregion Properties
 
-        #region Initialize
+        private const int NUMBER_OF_ROLES = 3;
+        private const string ROLE_ID = "35";
+        private const string ROLE_NAME = "Developer";
+        private const int NUMBER_OF_ROLE_PERMISSIONS = 29;
+
         [TestInitialize]
         public void Initialize()
         {
-            uri = ConfigurationManager.AppSettings["uri"];
-            apiKey = ConfigurationManager.AppSettings["apiKey"];
-
             SetMimeTypeJSON();
             SetMimeTypeXML();
         }
@@ -41,32 +31,38 @@ namespace UnitTest_redmine_net40_api
         [Conditional("JSON")]
         private void SetMimeTypeJSON()
         {
-            redmineManager = new RedmineManager(uri, apiKey, MimeFormat.json);
+            redmineManager = new RedmineManager(Helper.Uri, Helper.ApiKey, MimeFormat.json);
         }
 
         [Conditional("XML")]
         private void SetMimeTypeXML()
         {
-            redmineManager = new RedmineManager(uri, apiKey, MimeFormat.xml);
+            redmineManager = new RedmineManager(Helper.Uri, Helper.ApiKey, MimeFormat.xml);
         }
-        #endregion Initialize
 
-        #region Tests
         [TestMethod]
-        public void RedmineRoles_ShouldGetAllRoles()
+        public void Should_Get_All_Roles()
         {
             var roles = redmineManager.GetObjects<Role>(null);
 
-            Assert.IsTrue(roles.Count == numberOfRoles);
+            Assert.IsNotNull(roles, "Get all roles returned null");
+            Assert.IsTrue(roles.Count == NUMBER_OF_ROLES, "Roles count != " + NUMBER_OF_ROLES);
+            CollectionAssert.AllItemsAreNotNull(roles, "Roles list contains null items.");
+            CollectionAssert.AllItemsAreUnique(roles, "Roles items are not unique.");
+            CollectionAssert.AllItemsAreInstancesOfType(roles, typeof(Role), "Not all items are of type role.");
         }
 
         [TestMethod]
-        public void RedmineRoles_ShouldGetRoleById()
+        public void Should_Get_Role_By_Id()
         {
-            Role adminRole = redmineManager.GetObject<Role>(roleId, null);
+            Role role = redmineManager.GetObject<Role>(ROLE_ID, null);
 
-            Assert.AreEqual(adminRole.Name, roleName);
+            Assert.IsNotNull(role, "Get role returned null.");
+            Assert.AreEqual(role.Name, ROLE_NAME, "Role name is invalid."); ;
+            Assert.IsNotNull(role.Permissions, "Role permisions list is null.");
+            CollectionAssert.AllItemsAreNotNull(role.Permissions.ToList(), "Permissions contains null items.");
+            CollectionAssert.AllItemsAreUnique(role.Permissions.ToList(), "Permissions are not unique.");
+            Assert.IsTrue(role.Permissions.Count == NUMBER_OF_ROLE_PERMISSIONS, "Permissions count != "+NUMBER_OF_ROLE_PERMISSIONS);
         }
-        #endregion Tests
     }
 }
