06e23fe8f7be97340f4ed97a01ecc8514ff75383
Alina Chitu <alinaiuliana.chitu@gmail.com>
2016-01-24 13:20:53 +0200

    Updated wiki page tests

--- UnitTest-redmine-net40-api/WikiPageTests.cs
+++ UnitTest-redmine-net40-api/WikiPageTests.cs
@@ -15,29 +15,19 @@ namespace UnitTest_redmine_net40_api
     [TestClass]
     public class WikiPageTests
     {
-        #region Constants
-        private const string projectId = "9";
-        private const string wikiPageName = "Wiki";
-        private const int noOfWikiPages = 1;
-        private const int wikiPageVersion = 1;
-
-        private const string wikiPageUpdatedText = "Updated again and again wiki page";
-        private const string wikiPageComment = "I did it through code";
-        #endregion Constants
-
-        #region Properties
         private RedmineManager redmineManager;
-        private string uri;
-        private string apiKey;
-        #endregion Properties
 
-        #region Initialize
+        private const string PROJECT_ID = "redmine-test";
+        private const string WIKI_PAGE_NAME = "Wiki";
+        private const string WIKI_PAGE_UPDATED_TEXT = "Updated again and again wiki page";
+        private const string WIKI_PAGE_COMMENT = "I did it through code";
+
+        private const int NUMBER_OF_WIKI_PAGES = 1;
+        private const int WIKI_PAGE_VERSION = 1;
+       
         [TestInitialize]
         public void Initialize()
         {
-            uri = ConfigurationManager.AppSettings["uri"];
-            apiKey = ConfigurationManager.AppSettings["apiKey"];
-
             SetMimeTypeJSON();
             SetMimeTypeXML();
         }
@@ -45,58 +35,89 @@ namespace UnitTest_redmine_net40_api
         [Conditional("JSON")]
         private void SetMimeTypeJSON()
         {
-            redmineManager = new RedmineManager(uri, apiKey, MimeFormat.json);
+            redmineManager = new RedmineManager(Helper.Uri, Helper.ApiKey, MimeFormat.json);
         }
 
         [Conditional("XML")]
         private void SetMimeTypeXML()
         {
-            redmineManager = new RedmineManager(uri, apiKey, MimeFormat.xml);
+            redmineManager = new RedmineManager(Helper.Uri, Helper.ApiKey, MimeFormat.xml);
         }
-        #endregion Initialize
-
-        #region Tests
+      
         [TestMethod]
-        public void RedmineWikiPages_ShouldAddOrUpdatePage()
+        public void Should_Add_Or_Update_WikiPage()
         {
-            WikiPage page = redmineManager.CreateOrUpdateWikiPage(projectId, wikiPageName, new WikiPage { Text = wikiPageUpdatedText, Comments = wikiPageComment });
+            WikiPage page = redmineManager.CreateOrUpdateWikiPage(PROJECT_ID, WIKI_PAGE_NAME, new WikiPage { Text = WIKI_PAGE_UPDATED_TEXT, Comments = WIKI_PAGE_COMMENT });
 
-            Assert.IsTrue(page.Title == wikiPageName);
-        }
-
-        [TestMethod]
-        public void RedmineWikiPages_ShouldGetAllPages()
-        {
-            List<WikiPage> pages = (List<WikiPage>)redmineManager.GetAllWikiPages(projectId);
-
-            Assert.IsTrue(pages.Count == noOfWikiPages && pages[0].Title.Equals(wikiPageName));
+            Assert.IsNotNull(page, "Wiki page is null.");
+            Assert.AreEqual(page.Title, WIKI_PAGE_NAME, "Wiki page name is invalid.");
+            Assert.AreEqual(page.Text, WIKI_PAGE_UPDATED_TEXT, "Wiki page text is invalid.");
+            Assert.AreEqual(page.Comments, WIKI_PAGE_COMMENT, "Wiki page comments are invalid.");
         }
 
         [TestMethod]
-        public void RedmineWikiPages_ShouldReturnWikiPage()
+        public void Should_Get_All_Wiki_Pages_By_Project_Id()
         {
-            WikiPage page = redmineManager.GetWikiPage(projectId, new NameValueCollection { { "include", "attachments" } }, wikiPageName);
+            List<WikiPage> pages = (List<WikiPage>)redmineManager.GetAllWikiPages(PROJECT_ID);
 
-            Assert.IsTrue(page.Title == wikiPageName);
+            Assert.IsNotNull(pages, "Wiki pages list is null.");
+            CollectionAssert.AllItemsAreNotNull(pages, "Wiki pages list contains null elements.");
+            CollectionAssert.AllItemsAreUnique(pages, "Wiki pages are not unique.");
+            CollectionAssert.AllItemsAreInstancesOfType(pages, typeof(WikiPage), "Not all pages are of type WikiPage.");
+            Assert.IsTrue(pages.Count == NUMBER_OF_WIKI_PAGES, "Wiki pages count != "+NUMBER_OF_WIKI_PAGES);
+            Assert.IsTrue(pages.Exists(p => p.Title == WIKI_PAGE_NAME), string.Format("Wiki page {0} does not exist", WIKI_PAGE_NAME));
         }
 
         [TestMethod]
-        public void RedmineWikiPages_ShouldReturnOldVersion()
+        public void Should_Get_Wiki_Page_By_Title()
         {
-            WikiPage oldPage = redmineManager.GetWikiPage(projectId, new NameValueCollection { { "include", "attachments" } }, wikiPageName, wikiPageVersion);
+            WikiPage page = redmineManager.GetWikiPage(PROJECT_ID, null, WIKI_PAGE_NAME);
 
-            Assert.IsTrue(oldPage.Title == wikiPageName);
-            Assert.IsTrue(oldPage.Version == wikiPageVersion);
+            Assert.IsNotNull(page, "Wiki page is null.");
+            Assert.AreEqual(page.Title, WIKI_PAGE_NAME, "Wiki page name is invalid.");
         }
 
         [TestMethod]
-        public void RedmineWikiPages_ShouldDelete()
+        public void Should_Get_Wiki_Page_By_Title_With_Attachments()
         {
-            redmineManager.DeleteWikiPage(projectId, wikiPageName);
+            WikiPage page = redmineManager.GetWikiPage(PROJECT_ID, new NameValueCollection { { RedmineKeys.INCLUDE, RedmineKeys.ATTACHMENTS } }, WIKI_PAGE_NAME);
+
+            Assert.IsNotNull(page, "Wiki page is null.");
+            Assert.AreEqual(page.Title, WIKI_PAGE_NAME, "Wiki page name is invalid.");
+            Assert.IsNotNull(page.Attachments.ToList(), "Attachments list is null.");
+            CollectionAssert.AllItemsAreNotNull(page.Attachments.ToList(), "Wiki page attachments list contains null elements.");
+            CollectionAssert.AllItemsAreUnique(page.Attachments.ToList(), "Wiki page attachments are not unique.");
+            CollectionAssert.AllItemsAreInstancesOfType(page.Attachments.ToList(), typeof(Attachment), "Not all attachments are of type Attachment.");
+        }
+
+        [TestMethod]
+        public void Should_Get_Wiki_Page_By_Version()
+        {
+            WikiPage oldPage = redmineManager.GetWikiPage(PROJECT_ID,null, WIKI_PAGE_NAME, WIKI_PAGE_VERSION);
+
+            Assert.IsNotNull(oldPage, "Wiki page is null.");
+            Assert.AreEqual(oldPage.Title, WIKI_PAGE_NAME, "Wiki page name is invalid.");
+            Assert.IsTrue(oldPage.Version == WIKI_PAGE_VERSION, "Wiki page version is invalid.");
+        }
+
+        [TestMethod]
+        public void Should_Compare_Wiki_Pages()
+        {
+            WikiPage page = redmineManager.GetWikiPage(PROJECT_ID, new NameValueCollection { { RedmineKeys.INCLUDE, RedmineKeys.ATTACHMENTS } }, WIKI_PAGE_NAME);
+            WikiPage pageToCompare = redmineManager.GetWikiPage(PROJECT_ID, new NameValueCollection { { RedmineKeys.INCLUDE, RedmineKeys.ATTACHMENTS } }, WIKI_PAGE_NAME);
+
+            Assert.IsNotNull(page, "Wiki page is null.");
+            Assert.IsTrue(page.Equals(pageToCompare), "Wiki pages are not equal.");
+        }
+
+        [TestMethod]
+        public void Should_Delete_Wiki_Page()
+        {
+            redmineManager.DeleteWikiPage(PROJECT_ID, WIKI_PAGE_NAME);
 
             try
             {
-                WikiPage page = redmineManager.GetWikiPage(projectId, null, wikiPageName);
+                WikiPage page = redmineManager.GetWikiPage(PROJECT_ID, null, WIKI_PAGE_NAME);
             }
             catch (RedmineException exc)
             {
@@ -106,15 +127,5 @@ namespace UnitTest_redmine_net40_api
             Assert.Fail("Test failed");
 
         }
-
-        [TestMethod]
-        public void RedmineWikiPages_ShouldCompare()
-        {
-            WikiPage page = redmineManager.GetWikiPage(projectId, new NameValueCollection { { "include", "attachments" } }, wikiPageName);
-            WikiPage pageToCompare = redmineManager.GetWikiPage(projectId, new NameValueCollection { { "include", "attachments" } }, wikiPageName);
-
-            Assert.IsTrue(page.Equals(pageToCompare));
-        }
-        #endregion Tests
     }
 }
