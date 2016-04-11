4c4812be3a16d5524e236bd63332c220ec82ca0f
Oliver Luethi <oliver.luethi@swisscom.com>
2016-04-11 15:17:52 +0200

    last stand

---
+++ redmine-net40-api/Extensions/JObjectExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Redmine.Net.Api.JSonConverters;
+using Redmine.Net.Api.Types;
+
+namespace Redmine.Net.Api.Extensions
+{
+    public static class JObjectExtensions
+    {
+        public static IdentifiableName GetValueAsIdentifiableName(this JObject obj, string key)
+        {
+            JToken val;
+
+            if (!obj.TryGetValue(key, out val)) return null;
+
+            //var ser = new JavaScriptSerializer();
+            //ser.RegisterConverters(new[] { new IdentifiableNameConverter() });
+
+            //var result = ser.ConvertToType<IdentifiableName>(val);
+            //return result;
+
+            return val.ToObject<IdentifiableName>();
+        }
+
+        public static List<T> GetValueAsCollection<T>(this JObject obj, string key) where T : new()
+        {
+            JToken val;
+
+            if (!obj.TryGetValue(key, out val)) return null;
+
+            //var ser = new JavaScriptSerializer();
+            //ser.RegisterConverters(new[] { RedmineSerializer.JsonConverters[typeof(T)] });
+
+            //var list = new List<T>();
+
+            //var arrayList = val as ArrayList;
+            //if (arrayList != null)
+            //{
+            //    list.AddRange(from object item in arrayList select ser.ConvertToType<T>(item));
+            //}
+            //else
+            //{
+            //    var dict = val as Dictionary<string, object>;
+            //    if (dict != null)
+            //    {
+            //        list.AddRange(dict.Select(pair => ser.ConvertToType<T>(pair.Value)));
+            //    }
+            //}
+            //return list;
+
+            return null;
+        }
+    }
+}
