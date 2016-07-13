96120009cd29e50b4bd983a41637622f69f0e8ca
Adrian Popescu <adi.popescu@gmail.com>
2016-07-13 22:11:52 +0200

    Added comments.

--- redmine-net20-api/RedmineWebClient.cs
+++ redmine-net20-api/RedmineWebClient.cs
@@ -20,13 +20,66 @@ using System.Net;
 namespace Redmine.Net.Api
 {
     /// <summary>
-    /// 
     /// </summary>
     public class RedmineWebClient : WebClient
     {
-        const string UA = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:8.0) Gecko/20100101 Firefox/8.0";
-        //  private readonly CookieContainer container = new CookieContainer();
+        private const string UA = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:8.0) Gecko/20100101 Firefox/8.0";
 
+        /// <summary>
+        ///     Gets or sets a value indicating whether [use proxy].
+        /// </summary>
+        /// <value>
+        ///     <c>true</c> if [use proxy]; otherwise, <c>false</c>.
+        /// </value>
+        public bool UseProxy { get; set; }
+
+        /// <summary>
+        ///     Gets or sets a value indicating whether [use cookies].
+        /// </summary>
+        /// <value>
+        ///     <c>true</c> if [use cookies]; otherwise, <c>false</c>.
+        /// </value>
+        public bool UseCookies { get; set; }
+
+        /// <summary>
+        ///     in miliseconds
+        /// </summary>
+        /// <value>
+        ///     The timeout.
+        /// </value>
+        public TimeSpan? Timeout { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the cookie container.
+        /// </summary>
+        /// <value>
+        ///     The cookie container.
+        /// </value>
+        public CookieContainer CookieContainer { get; set; }
+
+        /// <summary>
+        ///     Gets or sets a value indicating whether [pre authenticate].
+        /// </summary>
+        /// <value>
+        ///     <c>true</c> if [pre authenticate]; otherwise, <c>false</c>.
+        /// </value>
+        public bool PreAuthenticate { get; set; }
+
+        /// <summary>
+        ///     Gets or sets a value indicating whether [keep alive].
+        /// </summary>
+        /// <value>
+        ///     <c>true</c> if [keep alive]; otherwise, <c>false</c>.
+        /// </value>
+        public bool KeepAlive { get; set; }
+
+        /// <summary>
+        ///     Returns a <see cref="T:System.Net.WebRequest" /> object for the specified resource.
+        /// </summary>
+        /// <param name="address">A <see cref="T:System.Uri" /> that identifies the resource to request.</param>
+        /// <returns>
+        ///     A new <see cref="T:System.Net.WebRequest" /> object for the specified resource.
+        /// </returns>
         protected override WebRequest GetWebRequest(Uri address)
         {
             var wr = base.GetWebRequest(address);
@@ -34,13 +87,13 @@ namespace Redmine.Net.Api
 
             if (httpWebRequest != null)
             {
-
                 if (UseCookies)
                 {
                     httpWebRequest.Headers.Add(HttpRequestHeader.Cookie, "redmineCookie");
                     httpWebRequest.CookieContainer = CookieContainer;
                 }
-                httpWebRequest.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate | DecompressionMethods.None;
+                httpWebRequest.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate |
+                                                        DecompressionMethods.None;
                 httpWebRequest.PreAuthenticate = PreAuthenticate;
                 httpWebRequest.KeepAlive = KeepAlive;
                 httpWebRequest.UseDefaultCredentials = UseDefaultCredentials;
@@ -65,17 +118,5 @@ namespace Redmine.Net.Api
 
             return base.GetWebRequest(address);
         }
-
-        public bool UseProxy { get; set; }
-        public bool UseCookies { get; set; }
-
-        /// <summary>
-        /// in miliseconds
-        /// </summary>
-        public TimeSpan? Timeout { get; set; }
-        public CookieContainer CookieContainer { get; set; }
-        public bool PreAuthenticate { get; set; }
-        public bool KeepAlive { get; set; }
-
     }
 }
