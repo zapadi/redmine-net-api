4f668212b6503da65241854e5265c11092cb47aa
Zapadi <adi.popescu@gmail.com>
2018-03-29 00:13:42 +0300

    Added TestConsole project.

--- src/redmine-net-api.TestConsole/Program.cs
+++ src/redmine-net-api.TestConsole/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace redmine_net_api.TestConsole
+﻿namespace redmine.net.api.TestConsole
 {
     class Program
     {
