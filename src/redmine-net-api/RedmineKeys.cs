7b735fac1a31db36b41683715adfed3b76405aea
zapadi <adi.popescu@gmail.com>
2022-03-05 15:57:01 +0200

    Update copyright

--- src/redmine-net-api/RedmineKeys.cs
+++ src/redmine-net-api/RedmineKeys.cs
@@ -1,5 +1,5 @@
 /*
-   Copyright 2016 - 2017 Adrian Popescu.
+   Copyright 2011 - 2022 Adrian Popescu.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
