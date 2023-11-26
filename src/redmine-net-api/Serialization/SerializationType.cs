5a66e17b788cc0e51947078799e38dcaa9451092
zapadi <adi.popescu@gmail.com>
2023-11-26 19:18:22 +0200

    [New] SerializationType

---
+++ src/redmine-net-api/Serialization/SerializationType.cs
@@ -0,0 +1,16 @@
+namespace Redmine.Net.Api.Serialization
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum SerializationType
+    {
+        /// <summary>
+        /// </summary>
+        Xml,
+        /// <summary>
+        /// The json
+        /// </summary>
+        Json
+    }
+}
