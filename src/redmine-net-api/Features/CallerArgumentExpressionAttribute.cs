5b1b5ccf07607a341361e46008bca6dbe65a1bb2
zapadi <adi.popescu@gmail.com>
2023-11-28 22:38:14 +0200

    [ReSharper] Disable CheckNamespace

--- src/redmine-net-api/Features/CallerArgumentExpressionAttribute.cs
+++ src/redmine-net-api/Features/CallerArgumentExpressionAttribute.cs
@@ -4,7 +4,7 @@
 
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-
+// ReSharper disable once CheckNamespace
 namespace System.Runtime.CompilerServices
 {
     /// <summary>
--- src/redmine-net-api/Features/NotNullAttribute.cs
+++ src/redmine-net-api/Features/NotNullAttribute.cs
@@ -5,7 +5,7 @@
 
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-
+// ReSharper disable once CheckNamespace
 namespace System.Diagnostics.CodeAnalysis
 {
     /// <summary>
