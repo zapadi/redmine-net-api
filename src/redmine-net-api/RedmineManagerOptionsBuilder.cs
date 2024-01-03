2bfe7ed796de7342556330fdf2cf5e401ec7b714
zapadi <adi.popescu@gmail.com>
2024-01-03 17:55:05 +0200

    [RedmineManagerOptionsBuilder] Add Clienttype

--- src/redmine-net-api/RedmineManagerOptionsBuilder.cs
+++ src/redmine-net-api/RedmineManagerOptionsBuilder.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Net;
 using System.Xml.Serialization;
 using Redmine.Net.Api.Exceptions;
+using Redmine.Net.Api.Extensions;
+using Redmine.Net.Api.Net;
+using Redmine.Net.Api.Net.WebClient;
 using Redmine.Net.Api.Serialization;
 
 namespace Redmine.Net.Api
@@ -10,6 +14,12 @@ namespace Redmine.Net.Api
     /// </summary>
     public sealed class RedmineManagerOptionsBuilder
     {
+        private enum ClientType
+        {
+            None,
+            WebClient,
+        }
+        private ClientType _clientType = ClientType.None;
         
         /// <summary>
         /// 
@@ -32,24 +42,26 @@ namespace Redmine.Net.Api
         /// </summary>
         /// <param name="baseAddress"></param>
         /// <returns></returns>
-        public RedmineManagerOptionsBuilder WithBaseAddress(string baseAddress)
+        public RedmineManagerOptionsBuilder WithHost(string baseAddress)
         {
-            return WithBaseAddress(new Uri(baseAddress));
+            this.Host = baseAddress;
+            return this;
         }
 
         /// <summary>
         /// 
         /// </summary>
-        public Uri BaseAddress { get; private set; }
+        public string Host { get; private set; }
+        
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="baseAddress"></param>
+        /// <param name="mimeFormat"></param>
         /// <returns></returns>
-        public RedmineManagerOptionsBuilder WithBaseAddress(Uri baseAddress)
+        internal RedmineManagerOptionsBuilder WithSerializationType(MimeFormat mimeFormat)
         {
-            this.BaseAddress = baseAddress;
+            this.SerializationType = mimeFormat == MimeFormat.Xml ? SerializationType.Xml : SerializationType.Json;
             return this;
         }
         
@@ -90,8 +102,17 @@ namespace Redmine.Net.Api
         /// </summary>
         /// <param name="clientFunc"></param>
         /// <returns></returns>
-        public RedmineManagerOptionsBuilder WithClient(Func<IRedmineApiClient> clientFunc)
+        public RedmineManagerOptionsBuilder WithWebClient(Func<WebClient> clientFunc)
         {
+            if (clientFunc != null)
+            {
+                _clientType = ClientType.WebClient;
+            }
+
+            if (clientFunc == null && _clientType == ClientType.WebClient)
+            {
+                _clientType = ClientType.None;
+            }
             this.ClientFunc = clientFunc;
             return this;
         }
@@ -99,7 +120,7 @@ namespace Redmine.Net.Api
         /// <summary>
         /// 
         /// </summary>
-        public Func<IRedmineApiClient> ClientFunc { get; private set; }
+        public Func<WebClient> ClientFunc { get; private set; }
 
         /// <summary>
         /// 
@@ -150,38 +171,185 @@ namespace Redmine.Net.Api
         /// <returns></returns>
         internal RedmineManagerOptions Build()
         {
-            if (Authentication == null)
-            {
-                throw new RedmineException("Authentication cannot be null");
-            }
+            ClientOptions ??= new RedmineWebClientOptions();
+            
+            var baseAddress = CreateRedmineUri(Host, ClientOptions.Scheme);
             
             var options = new RedmineManagerOptions()
             {
-                PageSize = PageSize > 0 ? PageSize : RedmineManager.DEFAULT_PAGE_SIZE_VALUE,
+                BaseAddress = baseAddress,
+                PageSize = PageSize > 0 ? PageSize : RedmineConstants.DEFAULT_PAGE_SIZE_VALUE,
                 VerifyServerCert = VerifyServerCert,
-                Serializer = SerializationType == SerializationType.Xml ? new XmlRedmineSerializer() : new JsonRedmineSerializer(),
-                Version = Version,
-                //Authentication =
-                ClientOptions = ClientOptions,
-                
+                Serializer = RedmineSerializerFactory.CreateSerializer(SerializationType),
+                RedmineVersion = Version,
+                Authentication = Authentication,
+                ClientOptions = ClientOptions 
             };
-
             
             return options;
         }
         
         
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="serviceName"></param>
-        /// <param name="parts"></param>
-        /// <returns></returns>
-        public static bool TryParse(string serviceName, out string parts)
+        internal static void EnsureDomainNameIsValid(string domainName)
         {
-            parts = null;
-            return false;
+            if (domainName.IsNullOrWhiteSpace())
+            {
+                throw new RedmineException("Domain name cannot be null or empty.");
+            }
+    
+            if (domainName.Length > 255)
+            {
+                throw new RedmineException("Domain name cannot be longer than 255 characters.");
+            }
+
+            var labels = domainName.Split('.');
+            if (labels.Length == 1)
+            {
+                throw new RedmineException("Domain name is not valid.");
+            }
+            foreach (var label in labels)
+            {
+                if (label.IsNullOrWhiteSpace() || label.Length > 63)
+                {
+                    throw new RedmineException("Domain name must be between 1 and 63 characters.");
+                }
+                
+                if (!char.IsLetterOrDigit(label[0]) || !char.IsLetterOrDigit(label[label.Length - 1]))
+                {
+                    throw new RedmineException("Domain name starts or ends with a hyphen.");
+                }
+                
+                for (var i = 0; i < label.Length; i++)
+                {
+                    var c = label[i];
+
+                    if (!char.IsLetterOrDigit(c) && c != '-')
+                    {
+                        throw new RedmineException("Domain name contains an invalid character.");
+                    }
+
+                    if (c != '-')
+                    {
+                        continue;
+                    }
+                    
+                    if (i + 1 < label.Length && (c ^ label[i+1]) == 0)
+                    {
+                        throw new RedmineException("Domain name contains consecutive hyphens.");
+                    }
+                }
+            }
         }
         
+        internal static Uri CreateRedmineUri(string host, string scheme = null)
+        {
+            if (host.IsNullOrWhiteSpace() || host.Equals("string.Empty", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new RedmineException("The host is null or empty.");
+            }
+
+            if (!Uri.TryCreate(host, UriKind.Absolute, out var uri))
+            {
+                host = host.TrimEnd('/', '\\');
+                EnsureDomainNameIsValid(host);
+                
+                if (!host.StartsWith(Uri.UriSchemeHttp, StringComparison.OrdinalIgnoreCase) || !host.StartsWith(Uri.UriSchemeHttps, StringComparison.OrdinalIgnoreCase))
+                {
+                    host = $"{scheme ?? Uri.UriSchemeHttps}://{host}";
+
+                    if (!Uri.TryCreate(host, UriKind.Absolute, out uri))
+                    {
+                        throw new RedmineException("The host is not valid.");
+                    }
+                }
+            }
+            
+            if (!uri.IsWellFormedOriginalString())
+            {
+                throw new RedmineException("The host is not well-formed.");
+            }
+
+            scheme ??= Uri.UriSchemeHttps;
+            var hasScheme = false;
+            if (!uri.Scheme.IsNullOrWhiteSpace())
+            {
+                if (uri.Host.IsNullOrWhiteSpace() && uri.IsAbsoluteUri && !uri.IsFile)
+                {
+                    if (uri.Scheme.Equals("localhost", StringComparison.OrdinalIgnoreCase))
+                    {
+                        int port = 0;
+                        var portAsString = uri.AbsolutePath.RemoveTrailingSlash();
+                        if (!portAsString.IsNullOrWhiteSpace())
+                        {
+                            int.TryParse(portAsString, out port);
+                        }
+                             
+                        var ub = new UriBuilder(scheme, "localhost", port);
+                        return ub.Uri;
+                    }
+                }
+                else
+                {
+                    if (!IsSchemaHttpOrHttps(uri.Scheme))
+                    {
+                        throw new RedmineException("Invalid host scheme. Only HTTP and HTTPS are supported.");
+                    }
+
+                    hasScheme = true;
+                }
+            }
+            else
+            {
+                
+                if (!IsSchemaHttpOrHttps(scheme))
+                {
+                    throw new RedmineException("Invalid host scheme. Only HTTP and HTTPS are supported.");
+                }
+            }
+
+            var uriBuilder = new UriBuilder();
+            
+            if (uri.HostNameType == UriHostNameType.IPv6)
+            {
+                uriBuilder.Scheme = (hasScheme ? uri.Scheme : scheme ?? Uri.UriSchemeHttps);
+                uriBuilder.Host = uri.Host;
+            }
+            else
+            {
+                if (uri.Authority.IsNullOrWhiteSpace())
+                {
+                    if (uri.Port == -1)
+                    {
+                        if (int.TryParse(uri.LocalPath, out var port))
+                        {
+                            uriBuilder.Port = port;
+                        }
+                    }
+
+                    uriBuilder.Scheme = scheme ?? Uri.UriSchemeHttps;
+                    uriBuilder.Host = uri.Scheme;
+                }
+                else
+                {
+                    uriBuilder.Scheme = uri.Scheme;
+                    uriBuilder.Port = int.TryParse(uri.LocalPath, out var port) ? port : uri.Port;
+                    uriBuilder.Host = uri.Host;
+                }
+            }
+
+            try
+            {
+                return uriBuilder.Uri;
+            }
+            catch (Exception ex)
+            {
+                throw new RedmineException(ex.Message);
+            }
+        }
+
+        private static bool IsSchemaHttpOrHttps(string scheme)
+        {
+           return  scheme == Uri.UriSchemeHttp || scheme == Uri.UriSchemeHttps;
+        }
     }
 }
