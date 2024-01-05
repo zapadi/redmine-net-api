0907336609528ce6e912f8cf343067f062147cce
zapadi <adi.popescu@gmail.com>
2024-01-05 13:24:28 +0200

    [Rename] InternalRedmineWebClient to InternalwebClient

--- src/redmine-net-api/Net/WebClient/InternalWebClient.cs
+++ src/redmine-net-api/Net/WebClient/InternalWebClient.cs
@@ -1,3 +1,18 @@
+/*
+   Copyright 2011 - 2023 Adrian Popescu
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 using System;
 using System.Net;
 using Redmine.Net.Api.Exceptions;
@@ -5,15 +20,17 @@ using Redmine.Net.Api.Extensions;
 
 namespace Redmine.Net.Api.Net.WebClient;
 
-internal sealed class InternalRedmineWebClient : System.Net.WebClient
+internal sealed class InternalWebClient : System.Net.WebClient
 {
     private readonly IRedmineApiClientOptions _webClientSettings;
 
-    public InternalRedmineWebClient(RedmineManagerOptions redmineManagerOptions)
+    #pragma warning disable SYSLIB0014
+    public InternalWebClient(RedmineManagerOptions redmineManagerOptions)
     {
         _webClientSettings = redmineManagerOptions.ClientOptions;
         BaseAddress = redmineManagerOptions.BaseAddress.ToString();
     }
+    #pragma warning restore SYSLIB0014
 
     protected override WebRequest GetWebRequest(Uri address)
     {
