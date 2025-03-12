7b9cd707267ae04210c9c99c81c03a758ea641e3
zapadi <adi.popescu@gmail.com>
2025-03-12 22:38:48 +0200

    [Tests ] Add clonable tests

---
+++ tests/redmine-net-api.Tests/Clone/JournalCloneTests.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using Redmine.Net.Api.Types;
+using Xunit;
+
+namespace Padi.DotNet.RedmineAPI.Tests.Clone;
+
+public sealed class JournalCloneTests
+{
+    [Fact]
+    public void Clone_WithPopulatedProperties_ReturnsDeepCopy()
+    {
+        // Arrange
+        var journal = new Journal
+        {
+            Id = 1,
+            User = new IdentifiableName(1, "John Doe"),
+            Notes = "Test notes",
+            CreatedOn = DateTime.Now,
+            PrivateNotes = true,
+            Details = (List<Detail>)
+            [
+                new Detail
+                {
+                    Property = "status_id",
+                    Name = "Status",
+                    OldValue = "1",
+                    NewValue = "2"
+                }
+            ]
+        };
+
+        // Act
+        var clone = journal.Clone(false);
+
+        // Assert
+        Assert.NotNull(clone);
+        Assert.NotSame(journal, clone);
+        Assert.Equal(journal.Id, clone.Id);
+        Assert.Equal(journal.Notes, clone.Notes);
+        Assert.Equal(journal.CreatedOn, clone.CreatedOn);
+        Assert.Equal(journal.PrivateNotes, clone.PrivateNotes);
+
+        Assert.NotSame(journal.User, clone.User);
+        Assert.Equal(journal.User.Id, clone.User.Id);
+        Assert.Equal(journal.User.Name, clone.User.Name);
+
+        Assert.NotNull(clone.Details);
+        Assert.NotSame(journal.Details, clone.Details);
+        Assert.Equal(journal.Details.Count, clone.Details.Count);
+
+        var originalDetail = journal.Details[0];
+        var clonedDetail = clone.Details[0];
+        Assert.NotSame(originalDetail, clonedDetail);
+        Assert.Equal(originalDetail.Property, clonedDetail.Property);
+        Assert.Equal(originalDetail.Name, clonedDetail.Name);
+        Assert.Equal(originalDetail.OldValue, clonedDetail.OldValue);
+        Assert.Equal(originalDetail.NewValue, clonedDetail.NewValue);
+    }
+}
