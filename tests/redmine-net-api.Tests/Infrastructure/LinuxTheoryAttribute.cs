271393dc3148b52dc40dcd82e99f284099919ba4
zapadi <adi.popescu@gmail.com>
2025-03-12 22:49:24 +0200

    Add LinuxTheoryAttribute

---
+++ tests/redmine-net-api.Tests/Infrastructure/LinuxTheoryAttribute.cs
@@ -0,0 +1,15 @@
+using System.Runtime.InteropServices;
+using Xunit;
+
+namespace Padi.DotNet.RedmineAPI.Tests.Infrastructure;
+
+public class LinuxTheoryAttribute : TheoryAttribute
+{
+    public LinuxTheoryAttribute()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) == false)
+        {
+            Skip = "Test can be run only on Linux machine";
+        }
+    }
+}
