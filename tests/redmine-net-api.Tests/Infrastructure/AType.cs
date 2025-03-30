f9502788810b6d0acd0306c63a52c7abd5c5e9e7
zapadi <adi.popescu@gmail.com>
2025-03-30 23:03:44 +0300

    AType

---
+++ tests/redmine-net-api.Tests/Infrastructure/AType.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Padi.DotNet.RedmineAPI.Tests.Infrastructure;
+
+internal readonly struct A<T>{
+    public static A<T> Is => default;
+#pragma warning disable CS0184 // 'is' expression's given expression is never of the provided type
+    public static bool IsEqual<U>() => Is is A<U>;
+#pragma warning restore CS0184 // 'is' expression's given expression is never of the provided type
+    public static Type Value => typeof(T);                         
+                             
+}
