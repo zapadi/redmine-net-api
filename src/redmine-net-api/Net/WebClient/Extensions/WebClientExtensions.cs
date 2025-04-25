c038f82ff3e4557978d26836bb9e54504d152179
zapadi <adi.popescu@gmail.com>
2025-04-25 17:07:17 +0300

    WebClientExtensions

---
+++ src/redmine-net-api/Net/WebClient/Extensions/WebClientExtensions.cs
@@ -0,0 +1,30 @@
+using Redmine.Net.Api;
+using Redmine.Net.Api.Extensions;
+using Redmine.Net.Api.Net;
+using Redmine.Net.Api.Serialization;
+
+internal static class WebClientExtensions
+{
+    public static void ApplyHeaders(this System.Net.WebClient client, RequestOptions options, IRedmineSerializer serializer)
+    {
+        client.Headers.Add("Content-Type", options.ContentType ?? serializer.ContentType);
+
+        if (!options.UserAgent.IsNullOrWhiteSpace())
+        {
+            client.Headers.Add("User-Agent", options.UserAgent);
+        }
+
+        if (!options.ImpersonateUser.IsNullOrWhiteSpace())
+        {
+            client.Headers.Add(RedmineConstants.IMPERSONATE_HEADER_KEY, options.ImpersonateUser);
+        }
+
+        if (options.Headers is { Count: > 0 })
+        {
+            foreach (var header in options.Headers)
+            {
+                client.Headers.Add(header.Key, header.Value);
+            }
+        }
+    }
+}
