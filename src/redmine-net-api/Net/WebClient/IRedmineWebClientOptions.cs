6fa7643a3630f748ab23a06998367206f7184926
Padi <adi.popescu@gmail.com>
2025-05-11 15:25:47 +0300

    [IRedmineApiClientOptions] Remove redundant properties

--- src/redmine-net-api/Net/WebClient/IRedmineWebClientOptions.cs
+++ src/redmine-net-api/Net/WebClient/IRedmineWebClientOptions.cs
@@ -1,83 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Net.Cache;
-using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
+/*
+   Copyright 2011 - 2025 Adrian Popescu
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 namespace Redmine.Net.Api.Net.WebClient;
 
 /// <summary>
 /// 
 /// </summary>
-public interface IRedmineWebClientOptions : IRedmineApiClientOptions
-{
-#if NET40_OR_GREATER || NETCOREAPP
-    /// <summary>
-    /// 
-    /// </summary>
-    public X509CertificateCollection ClientCertificates { get;  set; }
-#endif
-        
-    /// <summary>
-    /// 
-    /// </summary>
-    int? DefaultConnectionLimit { get; set; }
-        
-    /// <summary>
-    /// 
-    /// </summary>
-    Dictionary<string, string> DefaultHeaders { get; set; }
-        
-    /// <summary>
-    /// 
-    /// </summary>
-    int? DnsRefreshTimeout { get; set; }
-        
-    /// <summary>
-    /// 
-    /// </summary>
-    bool? EnableDnsRoundRobin { get; set; }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    bool? KeepAlive { get; set; }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    int? MaxServicePoints { get; set; }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    int? MaxServicePointIdleTime { get; set; }
-        
-    /// <summary>
-    /// 
-    /// </summary>
-    RequestCachePolicy RequestCachePolicy { get; set; }
-        
-#if(NET46_OR_GREATER || NETCOREAPP)
-    /// <summary>
-    /// 
-    /// </summary>
-    public bool? ReusePort { get; set; }
-#endif
-        
-    /// <summary>
-    /// 
-    /// </summary>
-    RemoteCertificateValidationCallback ServerCertificateValidationCallback { get; set; }
-        
-    /// <summary>
-    /// 
-    /// </summary>
-    bool? UnsafeAuthenticatedConnectionSharing { get; set; }
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <remarks>Only HTTP/1.0 and HTTP/1.1 version requests are currently supported.</remarks>
-    Version ProtocolVersion { get; set; }
-}
+public interface IRedmineWebClientOptions : IRedmineApiClientOptions;
