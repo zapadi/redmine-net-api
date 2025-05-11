3e5a9614e2048ff82dbf87a4fc7eb593330c30c7
Padi <adi.popescu@gmail.com>
2025-05-11 15:12:54 +0300

    Split IRedmineApiClient into IAsync|SyncRedmineApiClient

--- src/redmine-net-api/Net/IRedmineApiClient.cs
+++ src/redmine-net-api/Net/IRedmineApiClient.cs
@@ -14,35 +14,14 @@
    limitations under the License.
 */
 
-using System.Threading;
-#if!(NET20)
-using System.Threading.Tasks;
-#endif
-
 namespace Redmine.Net.Api.Net;
 
 /// <summary>
 /// 
 /// </summary>
-internal interface IRedmineApiClient
+internal interface IRedmineApiClient : ISyncRedmineApiClient
+#if !(NET20 || NET35)
+    , IAsyncRedmineApiClient
+#endif
 {
-    ApiResponseMessage Get(string address, RequestOptions requestOptions = null);
-    ApiResponseMessage GetPaged(string address, RequestOptions requestOptions = null);
-    ApiResponseMessage Create(string address, string payload, RequestOptions requestOptions = null);
-    ApiResponseMessage Update(string address, string payload, RequestOptions requestOptions = null);
-    ApiResponseMessage Patch(string address, string payload, RequestOptions requestOptions = null);
-    ApiResponseMessage Delete(string address, RequestOptions requestOptions = null);
-    ApiResponseMessage Upload(string address, byte[] data, RequestOptions requestOptions = null);
-    ApiResponseMessage Download(string address, RequestOptions requestOptions = null);
-    
-    #if !(NET20)
-    Task<ApiResponseMessage> GetAsync(string address, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
-    Task<ApiResponseMessage> GetPagedAsync(string address, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
-    Task<ApiResponseMessage> CreateAsync(string address, string payload, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
-    Task<ApiResponseMessage> UpdateAsync(string address, string payload, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
-    Task<ApiResponseMessage> PatchAsync(string address, string payload, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
-    Task<ApiResponseMessage> DeleteAsync(string address, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
-    Task<ApiResponseMessage> UploadFileAsync(string address, byte[] data, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
-    Task<ApiResponseMessage> DownloadAsync(string address, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
-    #endif
 }
