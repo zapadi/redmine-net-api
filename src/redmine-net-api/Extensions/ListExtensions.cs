8ceac85d228f2ade5adc97786a464ee931e4babd
Padi <adi.popescu@gmail.com>
2025-06-02 13:18:11 +0300

    Remove unused list extensions

--- src/redmine-net-api/Extensions/ListExtensions.cs
+++ src/redmine-net-api/Extensions/ListExtensions.cs
@@ -30,30 +30,6 @@ namespace Redmine.Net.Api.Extensions
         /// <param name="listToClone">The list to be cloned.</param>
         /// <param name="resetId">Specifies whether to reset the ID for each cloned item.</param>
         /// <returns>A new list containing cloned copies of the elements from the original list. Returns null if the original list is null.</returns>
-        public static IList<T> Clone<T>(this IList<T> listToClone, bool resetId) where T : ICloneable<T>
-        {
-            if (listToClone == null)
-            {
-                return null;
-            }
-
-            var clonedList = new List<T>(listToClone.Count);
-            
-            foreach (var item in listToClone)
-            {
-                clonedList.Add(item.Clone(resetId));
-            }
-
-            return clonedList;
-        }
-
-        /// <summary>
-        /// Creates a deep clone of the specified list.
-        /// </summary>
-        /// <typeparam name="T">The type of elements in the list. Must implement <see cref="ICloneable{T}"/>.</typeparam>
-        /// <param name="listToClone">The list to be cloned.</param>
-        /// <param name="resetId">Specifies whether to reset the ID for each cloned item.</param>
-        /// <returns>A new list containing cloned copies of the elements from the original list. Returns null if the original list is null.</returns>
         public static List<T> Clone<T>(this List<T> listToClone, bool resetId) where T : ICloneable<T>
         {
             if (listToClone == null)
@@ -68,34 +44,6 @@ namespace Redmine.Net.Api.Extensions
                 clonedList.Add(item.Clone(resetId));
             }
             return clonedList;
-        }
-
-        /// <summary>
-        /// Compares two lists for equality by checking if they contain the same elements in the same order.
-        /// </summary>
-        /// <typeparam name="T">The type of elements in the lists. Must be a reference type.</typeparam>
-        /// <param name="list">The first list to be compared.</param>
-        /// <param name="listToCompare">The second list to be compared.</param>
-        /// <returns>True if both lists contain the same elements in the same order; otherwise, false. Returns false if either list is null.</returns>
-        public static bool Equals<T>(this IList<T> list, IList<T> listToCompare) where T : class
-        {
-            if (list == null || listToCompare == null)
-            {
-                return false;
-            }
-
-            if (list.Count != listToCompare.Count)
-            {
-                return false;
-            }
-            
-            var index = 0;
-            while (index < list.Count && list[index].Equals(listToCompare[index]))
-            {
-                index++;
-            }
-
-            return index == list.Count;
         }
 
         /// <summary>
