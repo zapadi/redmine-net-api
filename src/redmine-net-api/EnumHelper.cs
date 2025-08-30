e93c89e0d2912787894ff9c347a921a6eaea3e47
Padi <adi.popescu@gmail.com>
2025-08-30 12:27:12 +0300

    feat(helpers): add EnumHelper

---
+++ src/redmine-net-api/EnumHelper.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Redmine.Net.Api;
+
+/// <summary>
+/// 
+/// </summary>
+public static class EnumHelper
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="ignoreCase"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static T Parse<T>(string value, bool ignoreCase) where T : struct
+    {
+#if NETFRAMEWORK
+        return (T)Enum.Parse(typeof(T), value, ignoreCase);
+#else
+        return Enum.Parse<T>(value, ignoreCase);
+#endif
+    }
+}
