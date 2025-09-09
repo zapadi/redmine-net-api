0b953bc55378c12b7a984a63526101b5a40f0a99
Padi <adi.popescu@gmail.com>
2025-09-09 18:27:54 +0300

    [Tests] Remove obsolete files

--- tests/redmine-net-api.Tests/Infrastructure/Fixtures/JsonSerializerFixture.cs
+++
@@ -1,9 +0,0 @@
-using Redmine.Net.Api.Serialization;
-
-namespace Padi.DotNet.RedmineAPI.Tests.Infrastructure.Fixtures;
-
-public sealed class JsonSerializerFixture
-{
-    internal IRedmineSerializer Serializer { get; private set; } = new JsonRedmineSerializer();
-    
-}
--- tests/redmine-net-api.Tests/Infrastructure/Fixtures/XmlSerializerFixture.cs
+++
@@ -1,8 +0,0 @@
-using Redmine.Net.Api.Serialization;
-
-namespace Padi.DotNet.RedmineAPI.Tests.Infrastructure.Fixtures;
-
-public sealed class XmlSerializerFixture
-{
-    internal IRedmineSerializer Serializer { get; private set; } = new XmlRedmineSerializer();
-}
