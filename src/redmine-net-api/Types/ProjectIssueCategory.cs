ca6b536d2f265dc5afacb01fc6a124b505556f07
Padi <adi.popescu@gmail.com>
2025-09-12 14:00:31 +0300

    [Feat] ProjectIssueCategory override read/write methods

--- src/redmine-net-api/Types/ProjectIssueCategory.cs
+++ src/redmine-net-api/Types/ProjectIssueCategory.cs
@@ -15,8 +15,11 @@
 */
 
 using System.Diagnostics;
+using System.Xml;
 using System.Xml.Serialization;
+using Newtonsoft.Json;
 using Redmine.Net.Api.Extensions;
+using Redmine.Net.Api.Serialization;
 
 namespace Redmine.Net.Api.Types
 {
@@ -36,6 +39,85 @@ namespace Redmine.Net.Api.Types
             : base(id, name)
         {
         }
+        
+        #region Implementation of IXmlSerialization
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="reader"></param>
+        public override void ReadXml(XmlReader reader)
+        {
+            reader.Read();
+            while (!reader.EOF)
+            {
+                if (reader.IsEmptyElement && !reader.HasAttributes)
+                {
+                    reader.Read();
+                    continue;
+                }
+
+                switch (reader.Name)
+                {
+                    case RedmineKeys.ID: Id = reader.ReadElementContentAsInt(); break;
+                    case RedmineKeys.NAME: Name = reader.ReadElementContentAsString(); break;
+                    default: reader.Read(); break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="writer"></param>
+        public override void WriteXml(XmlWriter writer)
+        {
+            writer.WriteElementString(RedmineKeys.ID, Id.ToInvariantString());
+            writer.WriteElementString(RedmineKeys.NAME, Name);
+        }
+        #endregion
+        
+        #region Implementation of IJsonSerialization
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="reader"></param>
+        public override void ReadJson(JsonReader reader)
+        {
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonToken.EndObject)
+                {
+                    return;
+                }
+
+                if (reader.TokenType != JsonToken.PropertyName)
+                {
+                    continue;
+                }
+
+                switch (reader.Value)
+                {
+                    case RedmineKeys.ID: Id = reader.ReadAsInt(); break;
+                    case RedmineKeys.NAME: Name = reader.ReadAsString(); break;
+                    default: reader.Read(); break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="writer"></param>
+        public override void WriteJson(JsonWriter writer)
+        {
+            using (new JsonObject(writer, RedmineKeys.ISSUE_CATEGORY))
+            {
+                writer.WriteProperty(RedmineKeys.ID, Id.ToInvariantString());
+                writer.WriteProperty(RedmineKeys.NAME, Name);
+            }
+        }
+        #endregion
 
         /// <summary>
         /// 
