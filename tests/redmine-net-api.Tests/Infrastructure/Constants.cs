ddaf9190c41e20b9ffd3b51564854c200821a9b3
Padi <adi.popescu@gmail.com>
2025-11-21 14:14:40 +0200

    [Tests] Remove unused constants

--- tests/redmine-net-api.Tests/Infrastructure/Constants.cs
+++ tests/redmine-net-api.Tests/Infrastructure/Constants.cs
@@ -3,8 +3,6 @@ namespace Padi.DotNet.RedmineAPI.Tests.Infrastructure;
 public static class Constants
 {
     public const string DeserializeCollection = "DeserializeCollection";
-    public const string JsonRedmineSerializerCollection = "JsonRedmineSerializerCollection";
-    public const string RedmineCollection = "RedmineCollection";
     public const string JsonNewtonsoft = "JsonNewtonsoft";
     public const string JsonSystemText = "JsonSystemText";
     public const string Xml = "Xml";
